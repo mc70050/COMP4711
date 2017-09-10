6b2294ea340141938700d7764f10ef26dc4dec3a
Michael Chen <mc70050@gmail.com>
2017-09-09 23:26:48 -0700

    Student Class

--- Student.php
+++ Student.php
@@ -16,10 +16,10 @@ class Student {
     
     // Constructor
     function __construct() {
-    $this->surname = '';
-    $this->first_name = '';
-    $this->emails = array();
-    $this->grades = array();
+        $this->surname = '';
+        $this->first_name = '';
+        $this->emails = array();
+        $this->grades = array();
     }
     
     function add_email($which, $address) {
@@ -28,5 +28,21 @@ class Student {
 
     function add_grade($grade) {
         $this->grades[] = $grade;
+    }
+    
+    function average() {
+        $total = 0;
+        foreach ($this->grades as $value)
+            $total += $value;
+        return $total / count($this->grades);
+    }
+    
+    function toString() {
+        $result = $this->first_name . ' ' . $this->surname;
+        $result .= ' ('.$this->average().")\n";
+        foreach($this->emails as $which=>$what)
+            $result .= $which . ': '. $what. "\n";
+        $result .= "\n";
+        return '<pre>'.$result.'</pre>';
     }
 }
